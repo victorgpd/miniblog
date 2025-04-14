aec198196497243caf1198e0f2f2977054007a61
victorgpd <victorguilherme2407@outlook.com>
2025-04-14 16:57:47 -0300

    conclusão

--- src/App.jsx
+++ src/App.jsx
@@ -43,15 +43,15 @@ function App() {
         <Header />
         <Main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/posts/:id" element={<Post />} />
-            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-            <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
-            <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-            <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" />} />
-            <Route path="/posts/edit/:id" element={user ? <EditPost /> : <Navigate to="/login" />} />
+            <Route path="/miniblog" element={<Home />} />
+            <Route path="/miniblog/about" element={<About />} />
+            <Route path="/miniblog/search" element={<Search />} />
+            <Route path="/miniblog/posts/:id" element={<Post />} />
+            <Route path="/miniblog/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/miniblog/register" element={!user ? <Register /> : <Navigate to="/" />} />
+            <Route path="/miniblog/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+            <Route path="/miniblog/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" />} />
+            <Route path="/miniblog/posts/edit/:id" element={user ? <EditPost /> : <Navigate to="/login" />} />
           </Routes>
         </Main>
         <Footer />
